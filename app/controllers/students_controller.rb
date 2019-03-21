c97fa297eee7db7144a6881bf39be82a69b55708
Ayana Bando <ayanaflax@yahoo.com>
2019-03-21 15:00:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,6 @@
 class StudentsController < ApplicationController
   def index
-<<<<<<< HEAD
     @students = Student.all
     respond_to :html
-=======
->>>>>>> d072e73044de7cddee5028f7b8ffbe736a78168c
   end
 end
